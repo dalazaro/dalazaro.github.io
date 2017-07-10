7eaba804f25b4e52506ffa0be56e32cd2c4224ed
dalazaro <daryl.lazaro@gmail.com>
2017-07-09 17:01:49 -0700

    integrated GitHub API; My Work displays feed for repository links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,49 @@
-// sanity check
-console.log("app.js linked.");
+// define globals
+console.log('sanity check');
 
-// let hasName = prompt("Please enter your name.");
-//
-// let isHuman = false;
-//
-// if (hasName !== true) {
-//   isHuman = true;
-//   alert("Thank you! Please proceed to my website.");
-// } else {
-//   hasName = prompt("Please try again.");
-// };
+//define endpoint
+var repoEndpoint = 'https://api.github.com/users/dalazaro/repos';
+
+
+//when info is gathered
+$(document).ready(function() {
+
+  //callback for listing quake data/markers
+  listRepos();
+
+});
+
+
+//function for gathering quake locations and marker settings
+function listRepos(dataResponse) {
+
+  $.ajax({
+
+    method: 'GET',
+    url: repoEndpoint,
+    dataType: 'json',
+    success: function(dataResponse){
+
+
+      //defining unnamed Object as variable
+      var repositories = dataResponse;
+      console.log(repositories);
+
+      //for each location, create a list item and marker
+      repositories.forEach(function listAndAnchor(repo) {
+
+        //List item of earthquake info
+        var link = repo.html_url;
+        var title = repo.name;
+        var date = repo.created_at.substr(0, 10);
+
+        $('#listOfRepositories').append(`<li><a class="body-link" href="${link}" target="blank">[${title}]</a> (${date})</li>`);
+
+
+      });
+
+    }
+
+  });
+
+};
